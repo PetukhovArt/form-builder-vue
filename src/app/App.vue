75a2eb197b27c932e8de22d42148e1c0051c095f
PetukhovArt <s1abak38@gmail.com>
2024-11-04 11:55:25 +0500

    init

---
+++ src/app/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <FormMain />
+      <!--      <router-view />-->
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+//
+import FormMain from "@/shared/lib/form-builder/FormMain.vue";
+</script>
